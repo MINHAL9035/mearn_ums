d4e6959a55643047c83194e4f7d786bfec3f17f5
MINHAL9035 <mhdminhal44@gmail.com>
2024-04-19 15:05:33 +0530

    login and signup done

---
+++ Frontend/src/screens/adminSreen/AdminHome.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import HeaderAdmin from '../../components/admincomponent/HeaderAdmin'
+import AdminHomeBody from '../../components/admincomponent/AdminHomeBody'
+
+const AdminHome = () => {
+    return (
+        <>
+            <HeaderAdmin />
+            <AdminHomeBody />
+
+        </>
+    )
+}
+
+export default AdminHome
---
+++ backend/server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import connectDB from './config/db.js';
+import cookieParser from 'cookie-parser';
+
+const port = process.env.PORT || 5000
+import userRoutes from './routes/userRoutes.js'
+import adminRoutes from './routes/adminRoute.js'
+
+connectDB()
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(cookieParser())
+
+app.use('/api/users', userRoutes)
+app.use('/api/admin',adminRoutes)
+
+app.get('/', (req, res) => {
+    res.send('server is ready')
+})
+
+app.use(notFound)
+app.use(errorHandler)
+
+
+
+app.listen(port, () => console.log(`serever started on port http://localhost:${port}`))
+
